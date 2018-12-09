f01c42d00ba2a46b65d22b0e948c6feb9d606e61
Kevin Lewis <kevlew02@gmail.com>
2018-12-09 17:22:06 -0500

    modified log file paths to match linux directory structure

--- main.cpp
+++ main.cpp
@@ -7,48 +7,52 @@ int main()
 {
 	BitBoard Board;
 	Board.NewBoard();
-	std::string Line;
+	std::string line;
+	std::string path(getenv("HOME"));
+	std::string path2 = path;
+	path += "/Desktop/log.txt";
+	path2 += "/Desktop/output.txt";
 	std::ofstream cmdLog;
 	std::ofstream boardOutput;
-	cmdLog.open("C:/Users/Kevin/Desktop/log.txt");
-	boardOutput.open("C:/Users/Kevin/Desktop/output.txt");
+	cmdLog.open(path);
+	boardOutput.open(path2);
 	
 	std::cout.setf(std::ios::unitbuf); //Makes sure that the outputs are sent immediately to the GUI
 	
-	while (std::getline(std::cin, Line)) {
+	while (std::getline(std::cin, line)) {
 		//Write to Files
-		cmdLog << Line << std::endl;
-		if (Line == "uci") {
+		cmdLog << line << std::endl;
+		if (line == "uci") {
 			std::cout << "Zip Engine" << std::endl; //Engine Name
 			std::cout << "Kevin Lewis" << std::endl; //Author Name
 			std::cout << "uciok" << std::endl; //Enables UCI
 		}
-		else if (Line == "quit") {
+		else if (line == "quit") {
 			std::cout << "Game Over" << std::endl;
 			break;
 		}
-		else if (Line == "isready") {
+		else if (line == "isready") {
 			std::cout << "readyok" << std::endl;
 		}
-		else if (Line == "ucinewgame") {
+		else if (line == "ucinewgame") {
 			Board.NewBoard();
 		}
 
-		if (Line.substr(0, 24) == "position startpos moves ") {
-			std::string moveW = Line.substr(Line.size() - 4);
-			std::string moveB = Line.substr(Line.size() - 9, 4);
+		if (line.substr(0, 24) == "position startpos moves ") {
+			std::string moveW = line.substr(line.size() - 4);
+			std::string moveB = line.substr(line.size() - 9, 4);
 			Board.ExecuteMove(moveB);
 			Board.PrintBoard(boardOutput);
 			Board.ExecuteMove(moveW);
 			Board.PrintBoard(boardOutput);
 		}
-		else if (Line == "startpos" || Line == "position startpos") {
+		else if (line == "startpos" || line == "position startpos") {
 			Board.setColor(true);
 		}
-		else if (Line == "stop") {
+		else if (line == "stop") {
 			; // nothing
 		}
-		else if (Line.substr(0, 3) == "go ") {
+		else if (line.substr(0, 3) == "go ") {
 			std::string move = Board.SelectMove();
 			cmdLog << "best move: " << move << std::endl;
 			std::cout << "bestmove " << move << std::endl;
